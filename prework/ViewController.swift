6e0a2bf519fbd8fe0b91930a479d36a9d700d9fd
cn0017 <cnie@colgate.edu>
2021-08-24 18:49:51 -0400

    Here is my first commit message

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        // Reset slider value to zero
+        sliderLabel.text = "0%"
+        slider.value = 0
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get total tip by multiplying
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update tipamount lable
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func sliderValueUpdate(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get total tip by multiplying
+        let tipPercentage = Int(slider.value)
+        print(tipPercentage)
+        let tip = bill * Double(tipPercentage)/100
+        let total = bill + tip
+        
+        //Update slider label
+        sliderLabel.text = "\(Int(tipPercentage))%"
+        
+        
+        // Update tipamount lable
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
