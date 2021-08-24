3fa7e3c25ee9f19fedb209fdbd8d6d7372eece10
cn0017 <cnie@colgate.edu>
2021-08-24 17:10:04 -0400

    Initial Commit

---
+++ prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework
+//
+//  Created by NieCan on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
